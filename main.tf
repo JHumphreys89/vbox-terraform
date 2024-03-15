3c4807b52c92c38b4eb4793538a6b0fc02c28e43
Jon Humphreys <115595085+JHumphreys89@users.noreply.github.com>
2024-03-15 13:19:23 -0400

    Provider test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     virtualbox = {
-      source = "/.terraform.d/plugins/terraform-provider-virtualbox"
+      source = "terra-farm/virtualbox"
       version = "0.2.2-alpha.1"
     }
   }
